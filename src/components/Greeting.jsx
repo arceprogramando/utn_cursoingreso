34c45e38dbe6120c232135e7cd9b3317d5ea7baa
Felipe Arce <arceprogramando@gmail.com>
2024-01-08 17:34:39 -0300

    ✨ Finish : Finish tutorial blog of astro

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'preact/hooks';
+
+export default function Greeting({messages}) {
+
+  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div>
+      <h3>{greeting} ¡Gracias por tu visita!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        Nuevo saludo
+      </button>
+    </div>
+  );
+}
