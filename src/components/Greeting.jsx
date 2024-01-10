3855dbbb9ce60b7ae68fd5c2b1c2b9702784bb67
Felipe Arce <arceprogramando@gmail.com>
2024-01-09 22:32:17 -0300

    ✨ Feature: Creation of project for Technical University Degree in Programming at UTN,  with continuation of Astro Practice Blog and tailwind added

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 
-export default function Greeting({messages}) {
-
-  const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
-
+export default function Greeting({ messages }) {
+  const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
   const [greeting, setGreeting] = useState(randomMessage());
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setGreeting(randomMessage());
+    }, 700);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div>
-      <h3>{greeting} ¡Gracias por tu visita!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
-        Nuevo saludo
-      </button>
+    <div className="max-w-sm mx-auto">
+      <div class="flex flex-col justify-center text-center ">
+
+        <p> {greeting} ¡Gracias por tu visita! ☝️  </p>
+        <p >Pueden aportar o dejar sus estrellitas en <span>
+        <a href="https://github.com/arceprogramando/astro-practice" className="text-teal-400">Github </a>
+          
+          </span> </p>
+      </div>
+      <div className="text-center">
+      </div>
+      
     </div>
+
   );
 }
