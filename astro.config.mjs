bbef40e877bbe568df112495a3bcd48d9b278ff7
Felipe Arce <arceprogramando@gmail.com>
2024-06-20 04:02:39 -0300

    feat(seo):update sitemap with astro documentation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact(), tailwind()]
+  site: 'https://utn-cursoingreso.vercel.app',
+  integrations: [preact(), tailwind(),sitemap()],
 });
