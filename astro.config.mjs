1fc8815a04da0a2285122de93d868eec389ea316
Felipe Arce <arceprogramando@gmail.com>
2024-07-18 17:15:32 -0300

    feat: creation of SideMenu and new Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://utn-cursoingreso.vercel.app',
-  integrations: [preact(), tailwind(),sitemap()],
+  integrations: [preact(), tailwind(), sitemap(), icon()]
 });
---
+++ src/data/layouts/links.ts
@@ -0,0 +1,13 @@
+export interface Link {
+    href: string;
+    title: string;
+}
+
+export const linksData: Link[] = [
+    { href: '/', title: 'Inicio' },
+    { href: '/correlativity/', title: 'Correlatividad' },
+    { href: '/admission/', title: 'Ingreso' },
+    { href: '/career', title: 'Carrera' },
+    { href: '/collaborators', title: 'Colaboradores' },
+    {href:'/tags', title:'Tags'} 
+];
