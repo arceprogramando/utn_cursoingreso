a2248358e562e6151adde7b88152d1dd8ee69513
Felipe Arce <arceprogramando@gmail.com>
2024-07-19 01:22:02 -0300

    feat:improve footer

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'preact/hooks';
 
 export default function Greeting({ messages }) {
-  const randomMessage = () =>
-    messages[Math.floor(Math.random() * messages.length)];
+  const randomMessage = () => messages[Math.floor(Math.random() * messages.length)];
   const [greeting, setGreeting] = useState(randomMessage());
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setGreeting(randomMessage());
-    }, 700);
+    }, 4000);
 
     return () => clearInterval(intervalId);
   }, []);
@@ -16,16 +15,13 @@ export default function Greeting({ messages }) {
   return (
     <div className="mx-auto">
       <div class="flex flex-col justify-center text-center ">
-        <p class="pb-4">
+        <p>
           Pueden aportar o dejar sus estrellitas en{' '}
           <span>
-            <a
-              href="https://github.com/arceprogramando/astro-practice"
-              className="underline uppercase font-bold hover:text-teal-600"
-            >
-              Github{' '}
+            <a href="https://github.com/arceprogramando/astro-practice" className="underline uppercase font-bold hover:text-teal-600" target="_blank">
+              Github{greeting}
             </a>
-          </span>{' '}
+          </span>
         </p>
       </div>
       <div className="text-center"></div>
