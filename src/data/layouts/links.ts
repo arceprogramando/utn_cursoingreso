c9289c1c919d81ce1b3e2e12752dc62d18e769fd
Felipe Arce <arceprogramando@gmail.com>
2024-07-19 13:14:43 -0300

    feat: delete "inicio" path and reorder links.

--- src/data/layouts/links.ts
+++ src/data/layouts/links.ts
@@ -1,13 +1,12 @@
 export interface Link {
-    href: string;
-    title: string;
+  href: string;
+  title: string;
 }
 
 export const linksData: Link[] = [
-    { href: '/', title: 'Inicio' },
-    { href: '/correlativity/', title: 'Correlatividad' },
-    { href: '/admission/', title: 'Ingreso' },
-    { href: '/career', title: 'Carrera' },
-    { href: '/collaborators', title: 'Colaboradores' },
-    {href:'/tags', title:'Tags'} 
+  { href: '/admission/', title: 'Ingreso' },
+  { href: '/career', title: 'Carrera' },
+  { href: '/correlativity/', title: 'Correlatividad' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/collaborators', title: 'Colaboradores' },
 ];
