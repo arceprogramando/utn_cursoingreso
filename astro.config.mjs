843531bbb74fbeb777c34d153c50ed624b23a5d9
Felipe Arce <arceprogramando@gmail.com>
2024-09-11 12:38:15 -0300

    feat: change md to mdx to use Latex/katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://utn-cursoingreso.vercel.app',
-  integrations: [preact(), tailwind(), sitemap(), icon()],
+  integrations: [preact(), tailwind(), sitemap(), icon(), mdx()]
 });
