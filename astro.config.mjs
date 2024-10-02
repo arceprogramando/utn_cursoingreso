cee29a7aa1d6ad52b75185eb69ffeb77a300d295
Felipe Arce <arceprogramando@gmail.com>
2024-10-02 01:12:25 -0300

    feat: delete tags sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,17 +11,28 @@ export default defineConfig({
     preact(),
     tailwind(),
     sitemap({
-      filter: (page) => {
-        const excludedPages = [
-          'https://utn-cursoingreso.vercel.app/tags',
-          'https://utn-cursoingreso.vercel.app/correlativity/',
-          'https://utn-cursoingreso.vercel.app/collaborators',
-        ];
-        const isExcluded = excludedPages.some((excluded) => page === excluded);
-        return !isExcluded;
-      }
-      
-    }),
+  filter: (page) => {
+    const excludedPages = [
+      'https://utn-cursoingreso.vercel.app/tags/apuntes/',
+      'https://utn-cursoingreso.vercel.app/tags/arquitectura%20y%20sistemas%20operativos/',
+      'https://utn-cursoingreso.vercel.app/tags/Base%20De%20Datos/',
+      'https://utn-cursoingreso.vercel.app/tags/Estad%C3%ADstica/',
+      'https://utn-cursoingreso.vercel.app/tags/Ingles/',
+      'https://utn-cursoingreso.vercel.app/tags/lectura%20comprensiva/',
+      'https://utn-cursoingreso.vercel.app/tags/lenguaje%20c/',
+      'https://utn-cursoingreso.vercel.app/tags/Lenguaje%20Csharp/',
+      'https://utn-cursoingreso.vercel.app/tags/matematica/',
+      'https://utn-cursoingreso.vercel.app/tags/organizacion%20Empresarial/',
+      'https://utn-cursoingreso.vercel.app/tags/Primer%20Cuatrimestre/',
+      'https://utn-cursoingreso.vercel.app/tags/Probabilidad/',
+      'https://utn-cursoingreso.vercel.app/tags/programacion/',
+      'https://utn-cursoingreso.vercel.app/tags/python/',
+      'https://utn-cursoingreso.vercel.app/tags/Segundo%20Cuatrimestre/',
+    ];
+    const isExcluded = excludedPages.some((excluded) => page === excluded);
+    return !isExcluded;
+  },
+}),
     icon(),
     mdx(),
   ],
