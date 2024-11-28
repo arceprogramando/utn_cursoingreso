7042e9287aa7a28d525aea6480b9c664142f2aac
Felipe Arce <arceprogramando@gmail.com>
2024-11-28 11:34:13 -0300

    feat: update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,13 +1,32 @@
 import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `
+
 User-agent: *
-Allow: /
+Disallow:
+
+User-agent: Googlebot
+Disallow:
 
 Sitemap: ${sitemapURL.href}
+
+Sitemap: https://utn-cursoingreso.vercel.app/sitemap-0.xml
+Sitemap: https://utn-cursoingreso.vercel.app/sitemap-index.xml
+
+User-agent: BadBot
+Disallow: /
+
+User-agent: *
+Allow: /public/
+
 `;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL('sitemap-index.xml', site);
-  return new Response(getRobotsTxt(sitemapURL));
+
+  return new Response(getRobotsTxt(sitemapURL), {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
 };
