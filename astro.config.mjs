1a3a506a4db4d51bb9914a4068d7b34329573de2
FelipeArce <arceprogramando@gmail.com>
2024-12-22 15:12:34 -0300

    fix: refactor excluded pages in sitemap configuration for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,25 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 
+const excludedPages = [
+  '/tags/',
+  '/tags/apuntes/',
+  '/tags/arquitectura-y-sistemas-operativos/',
+  '/tags/base-de-datos/',
+  '/tags/estadistica/',
+  '/tags/ingles/',
+  '/tags/lectura-comprensiva/',
+  '/tags/lenguaje-c/',
+  '/tags/lenguaje-csharp/',
+  '/tags/matematica/',
+  '/tags/organizacion-empresarial/',
+  '/tags/primer-cuatrimestre/',
+  '/tags/probabilidad/',
+  '/tags/programacion/',
+  '/tags/python/',
+  '/tags/segundo-cuatrimestre/',
+];
+
 export default defineConfig({
   site: 'https://utn-cursoingreso.vercel.app',
   integrations: [
@@ -12,36 +31,17 @@ export default defineConfig({
     tailwind(),
     sitemap({
       serialize(item) {
-        const excludedPages = [
-          '/tags/',
-          '/tags/apuntes/',
-          '/tags/arquitectura y sistemas operativos/',
-          '/tags/Base De Datos/',
-          '/tags/Estadística/',
-          '/tags/Ingles/',
-          '/tags/lectura comprensiva/',
-          '/tags/lenguaje c/',
-          '/tags/Lenguaje Csharp/',
-          '/tags/matematica/',
-          '/tags/organizacion Empresarial/',
-          '/tags/Primer Cuatrimestre/',
-          '/tags/Probabilidad/',
-          '/tags/programacion/',
-          '/tags/python/',
-          '/tags/Segundo Cuatrimestre/',
-        ];
-
-        if (excludedPages.some(page => item.url.includes(page))) {
+        if (excludedPages.some((page) => item.url.includes(page))) {
           return undefined;
         }
 
-        if (item.url === 'https://utn-cursoingreso.vercel.app/') {
+        if (item.url === '/') {
           item.changefreq = 'weekly';
           item.priority = 1;
-        } else if (item.url === 'https://utn-cursoingreso.vercel.app/career/') {
+        } else if (item.url === '/career/') {
           item.changefreq = 'weekly';
           item.priority = 0.9;
-        } else if (item.url === 'https://utn-cursoingreso.vercel.app/career/programacion2/') {
+        } else if (item.url === '/career/programacion2/') {
           item.changefreq = 'monthly';
           item.priority = 0.9;
         } else if (/\/career\//.test(item.url)) {
