bd77d8c8bfd4791fc259fe95add010735d86f003
FelipeArce <arceprogramando@gmail.com>
2024-12-22 02:47:32 -0300

    fix: improve site URL handling in robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,27 +1,27 @@
 import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `
-
 User-agent: *
-Disallow:
-
-User-agent: Googlebot
 Disallow:
 
 Sitemap: ${sitemapURL.href}
 
-Sitemap: https://utn-cursoingreso.vercel.app/sitemap-0.xml
-Sitemap: https://utn-cursoingreso.vercel.app/sitemap-index.xml
+User-agent: Googlebot
+Disallow:
 
 User-agent: BadBot
 Disallow: /
 
-User-agent: *
 Allow: /public/
-
 `;
 
 export const GET: APIRoute = ({ site }) => {
+  if (!site) {
+    return new Response('Site URL is not defined.', {
+      status: 500,
+    });
+  }
+
   const sitemapURL = new URL('sitemap-index.xml', site);
 
   return new Response(getRobotsTxt(sitemapURL), {
