a52e3a1c3f11d5b6073f20357008a9f1ea811c81
FelipeArce <arceprogramando@gmail.com>
2024-12-27 14:25:44 -0300

    chore: remove @astrojs/preact integration and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
@@ -8,7 +7,6 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://utn-cursoingreso.vercel.app',
   integrations: [
-    preact(),
     tailwind(),
     sitemap({
       serialize(item) {
