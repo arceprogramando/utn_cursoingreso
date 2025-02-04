0b9fdeaee07b84ae1232bb125257c75c3ecd3585
Felipe Arce <arceprogramando@gmail.com>
2025-02-04 13:44:36 -0300

    fix: update dependencies of tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			typography: {
-				DEFAULT: {
-					css: {
-						color: '#000',
-						a: {
-							color: '#3182ce',
-							'&:hover': {
-								color: '#2c5282',
-							},
-						},
-						h1: {
-							color: 'black'
-						},
-					},
-				},
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#000',
+            a: {
+              color: '#3182ce',
+              '&:hover': {
+                color: '#2c5282',
+              },
+            },
+            h1: {
+              color: 'black',
+            },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
 };
